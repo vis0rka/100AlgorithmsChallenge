fe1de408197172b5a8bad3decdac0305db3be295
Viktor <barta.viktor.ufs@gmail.com>
2019-04-30 17:21:33 +0200

    filter all longest string from array

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,15 @@
 function allLongestStrings(inputArray: string[]): string[] {
+  let firstSringLength = inputArray[0].length;
 
+  for (let i = 1; i < inputArray.length; i++) {
+
+    firstSringLength = inputArray[i].length > firstSringLength ? inputArray[i].length : firstSringLength;
+
+  }
+
+  return inputArray.filter(el => el.length >= firstSringLength);
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
+console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "abasd"]));
+console.log(allLongestStrings(["aba", "aadasda", "aadasdb", "vcd", "aba"]));
