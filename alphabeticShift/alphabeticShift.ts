9be7f0259254c3ac9c310ed61c728fee455619de
Viktor <barta.viktor.ufs@gmail.com>
2019-05-06 17:18:18 +0200

    change string character to +1 to alphabet

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,17 @@
-function alphabeticShift(inputString: string): string {
+const alphabet: any[] = ("abcdefghijklmnopqrstuvwxyz").split("");
 
+function alphabeticShift(inputString: string): any {
+  let tempArray = inputString.split("");
+  for (let i = 0; i < tempArray.length; i++) {
+    let tempNumb = 0;
+    
+    if (tempArray[i] !== 'z') {
+      tempNumb = alphabet.indexOf(tempArray[i]) + 1;
+    }
+    tempArray[i] = alphabet[tempNumb];
+  }
+
+  return tempArray.join('');
 }
 
 console.log(alphabeticShift('crazy'));
