4dbee986397ea487dc1c8d6951876194abdeea05
Viktor <barta.viktor.ufs@gmail.com>
2019-05-06 16:26:01 +0200

    almost increasing sequnce

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,15 @@
 function almostIncreasingSequence(sequence: number[]): boolean {
-
+  let counter: number = 0;
+  for (let i = 0; i < sequence.length; i++) {
+    if (sequence[i] < sequence[i - 1]) {
+      counter++;
+      if (sequence[i] <= sequence[i - 2] && sequence[i + 1] <= sequence[i - 1]) {
+        return false;
+      }
+    }
+  }
+  return counter <= 1;
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+console.log(almostIncreasingSequence([1, 3, 1, 1]))
+console.log(almostIncreasingSequence([1, 3, 2])) 
