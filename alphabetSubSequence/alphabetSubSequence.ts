cbb9cbbee53013dbd32f124b18501c48c1905166
Viktor <barta.viktor.ufs@gmail.com>
2019-05-06 19:07:23 +0200

    figure out is a string is a alphabetsubseq

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,5 +1,20 @@
 function alphabetSubsequence(s: string): boolean {
+  const chars: string[] = s.split("");
+  const charVals: number[] = [];
 
+  chars.forEach((char: string) => {
+    charVals.push(char.charCodeAt(0));
+  });
+
+  if (new Set(charVals).size !== charVals.length) {
+    return false;
+  }
+  for (let i = 0; i < charVals.length; i++) {
+    if (charVals[i] >= charVals[i + 1]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 console.log(alphabetSubsequence('zab'))
