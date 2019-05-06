315f425e72ee09c9378bc6f3197d054ff6b8dda2
Viktor <barta.viktor.ufs@gmail.com>
2019-05-06 19:12:48 +0200

    add odd and even index number total sum in array

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,15 @@
 function alternatingSums(a: number[]): number[] {
+  let totalWeights: number[] = [0,0]
+  a.forEach((element: number, index:number) => {
+    if (index % 2 == 0) {
+      totalWeights[0] += element;
+    } else {
+      totalWeights[1] += element;
+    }
 
+  });
+
+  return totalWeights;
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
