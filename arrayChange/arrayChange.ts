566ba9e49fc85a280cbe8a2c08560ff0a19944a0
Viktor <barta.viktor.ufs@gmail.com>
2019-05-06 19:54:25 +0200

    array change number one bye one to get a strictly increasing sequence

--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,14 @@
 function arrayChange(inputArray: number[]): number {
- 
+  let counter: number = 0;
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i] >= inputArray[i + 1]) {
+      let difference = ((inputArray[i] + 1) - inputArray[i + 1])
+      inputArray[i + 1] = inputArray[i] +1;
+      counter+=difference;
+    }
+  }
+  return counter;
 }
 
 console.log(arrayChange([1, 1, 1]));
+console.log(arrayChange([2, 1, 1]));
